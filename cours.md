5a4bb7df836c4d202776835c65f2bdb2c21bf6b2
Lucas <lulum366@gmail.com>
2024-02-20 12:59:46 +0100

    b1 js fetch json 20/02/2024

--- cours.md
+++ cours.md
@@ -1,4 +1,38 @@
 # Dévellopement web
 #=h1 ##=h2 etc... 
-README obligatoire
+README obligatoire en markdown
 Git : logiciel qui permet de gérer le code source et les ressource d'un site web
+## var, let et const
+var : permet de déclarer une variable
+let : permet de déclarer une seule fois une variable
+const : n'est pas modifiable
+
+## Sélectionner des éléments
+### Par Id :
+```
+let element = document.getElementById("monId");
+```
+### Par Classe :
+```
+let elements = document.getElementByClassName("maClasse");
+```
+### Par Balise :
+```
+let elements = document.getElementByTagName
+```
+
+## Modifier des éléments
+### Changer le texte :
+```
+document.getElementById("monId").textContent = "Nouveau texte";
+```
+
+## Ecouter et réagir aux évènements
+### Ajouter un écouteur d'évènements :
+```
+document.getElementById("monBouton").addEventListener("click", function (){
+    alert("Bouton cliqué");
+});
+```
+
+callback : fonction qui appelle en paramètre une autre fonction
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function changertitre() {
-    document.getElementById("titre").textContent = "Titre modifié !";
-}
+const monTitre = document.getElementById("titre");
 
-changertitre();
+
+
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", function() {
+    const firstNumb = Number(document.getElementById("a").value);
+    const secNumb = Number(document.getElementById("b").value);
+    const calc = firstNumb + secNumb
+    document.getElementById("result").innerHTML = calc
+});
+
+const subButton = document.getElementById("sub");
+subButton.addEventListener("click", function() {
+    const firstNumb = Number(document.getElementById("a").value);
+    const secNumb = Number(document.getElementById("b").value); 
+    const calc = firstNumb - secNumb
+    document.getElementById("result").innerHTML = calc
+});
+
+const multButton = document.getElementById("mult");
+multButton.addEventListener("click", function() {
+    const firstNumb = Number(document.getElementById("a").value);
+    const secNumb = Number(document.getElementById("b").value);
+    const calc = firstNumb * secNumb
+    document.getElementById("result").innerHTML = calc
+});
+
+const divButton = document.getElementById("div");
+divButton.addEventListener("click", function() {
+    const firstNumb = Number(document.getElementById("a").value);
+    const secNumb = Number(document.getElementById("b").value);
+    const calc = firstNumb / secNumb
+    document.getElementById("result").innerHTML = calc
+});
+
+const delButton = document.getElementById("del");
+delButton.addEventListener("click", function(){
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("a").value = "";
+    document.getElementById("b").value = ""
+});
